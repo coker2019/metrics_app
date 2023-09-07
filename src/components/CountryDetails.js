417cdcc63a06ed2db72be208c8212c2929c2df5d
coker2019 <olabisi2k11@gmail.com>
2023-09-07 17:44:47 +0100

    Styled components.

--- src/components/CountryDetails.js
+++ src/components/CountryDetails.js
@@ -9,21 +9,29 @@ const CountryDetails = () => {
   const index = countries.findIndex((country) => country.id === +id);
 
   return (
-    <div>
-      <h3>{countries[index].flag}</h3>
-      <h3>{countries[index].name}</h3>
+    <div className="details-container">
+      <div className="country-details">
+        <img src={countries[index].image} alt={countries[index].name} />
+        <h3 className="details-name">{countries[index].name}</h3>
 
-      <div>
-        <span>Population:</span>
-        <span>{countries[index].population}</span>
-      </div>
-      <div>
-        <span>Region:</span>
-        <span>{countries[index].region}</span>
-      </div>
-      <div>
-        <span>Country code:</span>
-        <span>{countries[index].id}</span>
+        <div className="details-list">
+          <div>
+            <span>Capital:</span>
+            <span>{countries[index].capital}</span>
+          </div>
+          <div>
+            <span>Population:</span>
+            <span>{countries[index].population}</span>
+          </div>
+          <div>
+            <span>Subregion:</span>
+            <span>{countries[index].subregion}</span>
+          </div>
+          <div>
+            <span>TimeZone:</span>
+            <span>{countries[index].timezone}</span>
+          </div>
+        </div>
       </div>
     </div>
   );
