6db30ad6475f81dc48024d7c198b4b0e8b6b4734
coker2019 <olabisi2k11@gmail.com>
2023-09-09 17:05:09 +0100

    Add countriesSlice test

---
+++ src/_test_/countriesSlice.test.js
@@ -0,0 +1,32 @@
+import countriesReducer, { fetchCountries, fetchCountry } from '../redux/countries/countriesSlice';
+
+// Import your reducer and initialState from the slice file
+import  { initialState } from '../redux/countries/countriesSlice';
+
+describe('countriesSlice', () => {
+  it('should handle fetchCountries.fulfilled', () => {
+    const action = {
+      type: fetchCountries.fulfilled.type,
+      payload: {
+        countries: [{ name: 'Country 1' }, { name: 'Country 2' }],
+      },
+    };
+
+    const newState = countriesReducer(initialState, action);
+
+    // Add your assertions here based on the expected state changes
+    expect(newState.countriesData).toEqual(action.payload.countries);
+  });
+
+  it('should handle fetchCountry.fulfilled', () => {
+    const action = {
+      type: fetchCountry.fulfilled.type,
+      payload: [{ name: 'Country 1' }],
+    };
+
+    const newState = countriesReducer(initialState, action);
+
+    // Add your assertions here based on the expected state changes
+    expect(newState.countriesData).toEqual(action.payload);
+  });
+});
